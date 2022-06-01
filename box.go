39aa25317d89d5fedb930e3d513825081a1d7866
hypnospaced <hypnospaced@yandex.ru>
2022-06-01 18:22:48 +0400

    Homework fix

--- box.go
+++ box.go
@@ -2,6 +2,10 @@ package golang_united_school_homework
 
 import "fmt"
 
+const (
+	outOfIndexErrMsg string = "[ERROR] Out of index or nil"
+)
+
 // box contains list of shapes and able to perform operations on them
 type box struct {
 	shapes         []Shape
@@ -19,7 +23,7 @@ func NewBox(shapesCapacity int) *box {
 // returns the error in case it goes out of the shapesCapacity range.
 func (b *box) AddShape(shape Shape) error {
 	if len(b.shapes) == b.shapesCapacity {
-		return fmt.Errorf("[ERROR] box is out of capacity.")
+		return fmt.Errorf("[ERROR] box is out of capacity")
 	} else {
 		b.shapes = append(b.shapes, shape)
 	}
@@ -31,7 +35,7 @@ func (b *box) AddShape(shape Shape) error {
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) GetByIndex(i int) (Shape, error) {
 	if i >= b.shapesCapacity || b.shapes[i] == nil {
-		return nil, fmt.Errorf("[ERROR] Out of index or nil.")
+		return nil, fmt.Errorf(outOfIndexErrMsg)
 	}
 	return b.shapes[i], nil
 	//panic("implement me")
@@ -42,7 +46,7 @@ func (b *box) GetByIndex(i int) (Shape, error) {
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ExtractByIndex(i int) (Shape, error) {
 	if i >= b.shapesCapacity || b.shapes[i] == nil {
-		return nil, fmt.Errorf("[ERROR] Out of index or nil.")
+		return nil, fmt.Errorf(outOfIndexErrMsg)
 	} else {
 		s := b.shapes[i]
 		b.shapes = append(b.shapes[:i], b.shapes[i+1:]...)
@@ -54,7 +58,7 @@ func (b *box) ExtractByIndex(i int) (Shape, error) {
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ReplaceByIndex(i int, shape Shape) (Shape, error) {
 	if i >= b.shapesCapacity || b.shapes[i] == nil {
-		return nil, fmt.Errorf("[ERROR] Out of index or nil.")
+		return nil, fmt.Errorf(outOfIndexErrMsg)
 	} else {
 		s := b.shapes[i]
 		b.shapes[i] = shape
@@ -89,6 +93,9 @@ func (b *box) RemoveAllCircles() error {
 		if _, ok := s.(Circle); ok {
 			indexList = append(indexList, i)
 		}
+	}
+	if len(indexList) == 0 {
+		return fmt.Errorf("[ERROR] No circles found")
 	}
 	for i := len(indexList) - 1; i >= 0; i-- {
 		_, err := b.ExtractByIndex(indexList[i])
